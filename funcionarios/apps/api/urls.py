0c0c832e2e1666f7ce89065477f6f32ea6130263
Asafe Oliveira <asafepy@gmail.com>
2018-12-31 10:09:51 -0200

    criando os dois microserviços

---
+++ funcionarios/apps/api/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path, include
+from rest_framework import routers, serializers, viewsets
+from apps.core.views import (LoginAuthToken, 
+                             ObtainAuthToken, 
+                             UserCreateViewSet)
+
+router = routers.DefaultRouter()
+
+# router.register(r'sensors', views.SensorViewSet)
+# router.register(r'equipments', views.EquipamentViewSet)
+# router.register(r'equipments_records', views.MeansurementRecordViewSet)
+
+helper_patterns = [
+    path('', include('rest_framework.urls', 
+                     namespace='rest_framework')),
+    path('user/', UserCreateViewSet.as_view(), name='user'),
+	path('api-token-auth/', ObtainAuthToken.as_view()),
+    path('authenticate/', 
+         LoginAuthToken.as_view(), 
+         name='authenticate'),
+]
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', 
+         include('rest_framework.urls', namespace='rest_framework')),
+]
+
+urlpatterns = helper_patterns
+urlpatterns.extend(router.urls)
