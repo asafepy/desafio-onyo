68e32ff9b8e8087c56c10d1295306d49fcd889df
Asafe Oliveira <asafepy@gmail.com>
2019-01-09 01:54:41 -0200

    credenciais DB

--- funcionarios/funcionarios/settings.py
+++ funcionarios/funcionarios/settings.py
@@ -102,9 +102,9 @@ WSGI_APPLICATION = 'funcionarios.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db', # set in docker-compose.yml
+        'NAME': 'root',
+        'USER': 'root',
+        'HOST': 'onyo', # set in docker-compose.yml
         'PORT': 5432 # default postgres port
     }
 }
